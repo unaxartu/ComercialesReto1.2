5ef1b3ca27a9defe0e7d202e256a19afbede1114
Aitor <lopezaitor726@gmail.com>
2025-01-09 09:38:48 +0100

    Layout Agenda

--- app/src/main/java/com/example/comercialesreto12/MainActivity.java
+++ app/src/main/java/com/example/comercialesreto12/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.comercialesreto12;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button botonAgenda = findViewById(R.id.agenda);
+        botonAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                agenda();
+            }
         });
+
+        View mainView = findViewById(R.id.main);
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
+
+    public void agenda() {
+        Intent i = new Intent(this, Agenda.class);
+        startActivity(i);
     }
 }
