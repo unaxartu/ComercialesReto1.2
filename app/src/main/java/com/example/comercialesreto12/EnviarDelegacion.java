1116371c9ada60555ef7518003a95d113db5f390
unaxartu <unaxergobi@gmail.com>
2025-01-17 09:38:18 +0100

    Merge Aitor and Unax branches to have a full fe app

--- app/src/main/java/com/example/comercialesreto12/EnviarDelegacion.java
+++ app/src/main/java/com/example/comercialesreto12/EnviarDelegacion.java
@@ -1,18 +1,17 @@
 package com.example.comercialesreto12;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
-public class EnviarDelegacion extends MainActivity{
+public class EnviarDelegacion extends MainActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enviar_delegacion);
+        setContentView(R.layout.activity_enviar_delegacion);  // Cargar el layout específico para esta actividad
 
-        // Llamar a la configuración del menú para asegurarse de que el header esté configurado
+        // Llamar a la configuración del menú heredado
         setupDrawerMenu();
-
     }
 }
